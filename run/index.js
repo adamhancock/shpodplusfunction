954e9128d2eb72bf86609558eeebad78da0c54d5
adammhancock@gmail.com <adammhancock@gmail.com>
2020-02-10 22:48:20 +0000

    added default for no namespace

--- run/index.js
+++ run/index.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 
 module.exports = async function(context, req) {
   const yaml = await fs.readFileSync('./yaml/shpod.yaml', 'utf8')
+  if (req.query.n == null) {
+    req.query.n == 'default'
+  }
   context.res = {
     body: yaml.replace('{ namespace }', req.query.n)
   }
